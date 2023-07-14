1d383edac751b3192977617a93fb5d0ecbc43f7c
Crowdbotics <engineering@crowdbotics.com>
2023-07-14 11:52:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wispy-waterfall-de-101425",
+  url: "https://wispy-waterfall-de-101425.botics.co",
+  api: "https://wispy-waterfall-de-101425.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
